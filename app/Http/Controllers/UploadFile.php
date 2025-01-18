0e862fd8f44251a21748184341fd5c23e88994c9
Tobin <tobin@cybridge.jp>
2025-01-18 22:50:04 +0700

    sast

--- app/Http/Controllers/UploadFile.php
+++ app/Http/Controllers/UploadFile.php
@@ -43,9 +43,7 @@ class UploadFile extends Controller
     {
         $unsafeInput = $request->input('search');
         $results = DB::select("SELECT * FROM users WHERE name = '$unsafeInput'");
+        
         return response()->json($results);
-
-        // $userInput = $_GET['username']; 
-        // $users = DB::select("SELECT * FROM users WHERE username = '$userInput'");
     }
 }
