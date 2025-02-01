598b95d97646bcd7a8e0104abfd5e5b4fb4da022
Tobin <tobin@cybridge.jp>
2025-02-01 12:36:56 +0700

    sast

--- app/Http/Controllers/UploadFile.php
+++ app/Http/Controllers/UploadFile.php
@@ -42,19 +42,7 @@ class UploadFile extends Controller
     
     
         public function authenticate() {
-            $user = $_POST['user'];
-            $pass = $_POST['pass';
-            $authenticated = false;
-    
-            $query = "SELECT * FROM users WHERE user = '" . $user . "' AND pass = '" . $pass . "'";
-    
-            $stmt = $this->conn->query($query); // Noncompliant
-    
-            if ($stmt->num_rows == 1) {
-              $authenticated = true;
-            }
-    
-            return $authenticated;
+            
         }
     
 }
